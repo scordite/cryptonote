2d4bd2fd656bad558003428ab0d8d9062476c4af
Scordite Blockchain <40725347+scordite@users.noreply.github.com>
2018-12-09 11:08:48 +0100

    Update version.cmake
    Update Warning Message

--- src/version.cmake
+++ src/version.cmake
@@ -1,6 +1,6 @@
 execute_process(COMMAND "${GIT}" describe --dirty --match "v${VERSION}" RESULT_VARIABLE RET OUTPUT_VARIABLE DESCRIPTION OUTPUT_STRIP_TRAILING_WHITESPACE)
 if(RET)
-    message(WARNING "Cannot determine current revision. Make sure that you are building either from a Git working tree or from a source archive.")
+    message(WARNING "Cannot determine current revision. Make sure that you are building either from a Git working tree or from a source archive. Visit https://github.com/scordite/scordite for more information.")
     set(VERSION "${COMMIT}")
     configure_file("src/version.h.in" "${TO}")
 else()
