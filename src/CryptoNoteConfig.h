6cb22279fff0ae6b8b701464834c0c511664b665
Scordite Blockchain <40725347+scordite@users.noreply.github.com>
2019-04-23 03:08:08 +0200

    added seed node

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -110,6 +110,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 const std::initializer_list<const char*> SEED_NODES = {
   "95.179.160.20:26111", //Frankfurt
   "95.179.141.233:26111", //Amsterdam
+  "165.22.67.156", //Paris
 };
 
 struct CheckpointData {
